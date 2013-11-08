1a653a61bd043fe61350523510bbc13b5c8eafeb
paul <psoucy@sciaps.com>
2013-11-08 12:03:35 -0500

    touchscreen now acts like a touchscreen and powerbutton acts like a powerbutton

--- device.mk
+++ device.mk
@@ -4,4 +4,6 @@ $(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
 
 PRODUCT_COPY_FILES += \
 	device/sciaps/marsgold/init.pcm049.rc:root/init.pcm049.rc \
-	device/sciaps/marsgold/fstab.pcm049:root/fstab.pcm049
+	device/sciaps/marsgold/fstab.pcm049:root/fstab.pcm049 \
+	device/sciaps/marsgold/stmpe-ts.idc:system/usr/idc/stmpe-ts.idc \
+	device/sciaps/marsgold/twl6030_pwrbutton.kl:system/usr/keylayout/twl6030_pwrbutton.kl
